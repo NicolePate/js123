930c6437ceb5d77f3746bf4bfece43057e7ab3f9
NicolePate <nicole.pate512@gmail.com>
2022-01-31 08:25:28 -0600

    low alert

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-function showNotification() {
-    const notification = new Notification ("new message", {
-        body: "client frustrated"
+function showAlert() {
+    const alter = new Notification ("new message", {
+        body: "inventory low"
     });
 }
 
 
 console.log(Notification.permission);
 
-if (Notification.permission === "granted") {
-    showNotification();
- } else if (Notification.permission !== "denied") {
-     Notification.requestPermission().then(permission => {
-        if (permission === "granted"){
-            showNofication();
-        }  
-     });
- }
+// if (Notification.permission === "granted") {
+//     showAlert();
+//  } else if (Notification.permission !== "denied") {
+//      Notification.requestPermission().then(permission => {
+//         if (permission === "granted"){
+//             showAlert();
+//         }  
+//      });
+//  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function submitButton() {
     alert("thank you for submitting");
@@ -42,10 +56,12 @@ document.querySelector(".minus-btn").addEventListener("click", function() {
     
     document.getElementById("quantity").value = valueCount;
 
-      if(valueCount == 1)
+      if(valueCount == 1){
+        showAlert();
+      }else if (valueCount === 0)
   {
     const para = document.createElement("p");
-    const node = document.createTextNode("Inventory is low!");
+    const node = document.createTextNode("Out of stock");
     para.appendChild(node);
     const element = document.getElementById("div1");
     element.appendChild(para);
